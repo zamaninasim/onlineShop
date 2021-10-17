d53220669031eb4c62a7f1a3f21a7d5eab1e0ad8
nasim <zamaninasim213@gamail.com>
2021-10-17 20:42:03 +0330

    add dao class

---
+++ src/ir/maktab/dao/Dao.java
@@ -0,0 +1,18 @@
+package ir.maktab.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Dao {
+    private Connection connection;
+
+    public Dao() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/taxi2", "root", "Nasim1374");
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+}
