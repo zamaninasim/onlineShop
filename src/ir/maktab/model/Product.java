d6761eb989cefe2b9548f158cc85a592002b73a5
nasim <zamaninasim213@gamail.com>
2021-10-18 16:44:29 +0330

    add constractor whithout id to product class

--- src/ir/maktab/model/Product.java
+++ src/ir/maktab/model/Product.java
@@ -1,7 +1,9 @@
 package ir.maktab.model;
 
 import ir.maktab.model.enumeration.ProductType;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 public class Product {
@@ -10,4 +12,11 @@ public class Product {
     private String name;
     private Long price;
     private Integer count;
+
+    public Product(ProductType productType, String name, Long price, Integer count) {
+        this.productType = productType;
+        this.name = name;
+        this.price = price;
+        this.count = count;
+    }
 }
