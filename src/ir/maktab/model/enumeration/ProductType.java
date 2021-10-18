a187ce8aa96e3560c014a54c1ee067174adb2337
nasim <zamaninasim213@gamail.com>
2021-10-18 10:26:57 +0330

    add productType enum

--- src/ir/maktab/model/enumeration/ProductType.java
+++ src/ir/maktab/model/enumeration/ProductType.java
@@ -1,2 +1,25 @@
-package ir.maktab.model.enumeration;public enum ProductType {
+package ir.maktab.model.enumeration;
+
+public enum ProductType {
+    ELECTRONICS(1, "electronics"),
+    SHOE(2, "shoe"),
+    READABLE(3, "readable"),
+    UNKNOWN(4, "unknown");
+
+    private Integer id;
+    private String name;
+
+    ProductType(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public ProductType getVal(String name) {
+        for (ProductType productType : values()) {
+            if (productType.name.equalsIgnoreCase(name.trim())) {
+                return productType;
+            }
+        }
+        return UNKNOWN;
+    }
 }
