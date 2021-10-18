a56688c441abdab1f950f2b003955f7529ed5d90
nasim <zamaninasim213@gamail.com>
2021-10-18 10:27:58 +0330

    add user class

--- src/ir/maktab/model/User.java
+++ src/ir/maktab/model/User.java
@@ -1,4 +1,17 @@
 package ir.maktab.model;
 
+import ir.maktab.model.enumeration.Gender;
+import lombok.Data;
+
+import java.sql.Date;
+
+@Data
 public class User {
+    private Integer id;
+    private String fullName;
+    private String phoneNumber;
+    private String email;
+    private Gender gender;
+    private Date birthDate;
+    private String nationalId;
 }
